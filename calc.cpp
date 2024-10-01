fa4941bf26d03a04252290caf2c40f2d505646b6
CViktoria <zaxar4ykvik@gmail.com>
2024-10-01 20:55:40 +0300

    Додано операцію додавання до калькулятора

---
+++ calc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, op;
+    double result;
+
+    cout << "Введіть перше число\n";
+    cin >> a;
+    cout << "Введіть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";
+    cin >> op;
+    cout << "Введіть друге число\n";
+    cin >> b;
+
+    switch (op) {
+    case 1: 
+        result = a + b; // код для додавання
+        break;
+    case 2: // код для віднімання
+        break;
+    case 3: // код для множення
+        break;
+    case 4: // код для ділення
+        break;
+    default:
+        cout << "Невірний оператор\n";
+        break;
+    }
+
+    cout << "Результат = " << result;
+}
